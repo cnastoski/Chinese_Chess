d94511075761df90a91f1ade0b7be0ba94cc823c
Chris <nastoski@msu.edu>
2021-05-11 17:38:05 -0400

    Added the users database model

--- xiangqi/game/models.py
+++ xiangqi/game/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=32)
+    email = models.CharField(max_length=64)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    password = models.CharField(max_length=64)
+    salt = models.CharField(max_length=16)
+
+
