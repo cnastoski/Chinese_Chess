99970f6b5e3d436a9c1a8e773d81e0a93b12121d
nova6919 <nnastoski1@gmail.com>
2021-05-13 15:29:21 -0400

    Added some js, correctly configured babel to build

--- xiangqi/game/static/src/js/index.js
+++ xiangqi/game/static/src/js/index.js
@@ -1,3 +1,5 @@
-import React from "react"
+import React from "react";
 import ReactDOM from "react-dom";
+import Game from "./component/game.js";
 
+ReactDOM.render(<Game />, document.getElementById("root"));
