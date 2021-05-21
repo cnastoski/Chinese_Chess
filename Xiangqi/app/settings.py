7d15e19ffea1c61175b1e0b19ae574235b36f694
nova6919 <nnastoski1@gmail.com>
2021-05-21 16:33:09 -0400

    secured

--- Xiangqi/app/settings.py
+++ Xiangqi/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .key import *
 import os
 
 
@@ -19,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t^qy@86w+(%7&t5o*p1ouii8qzo9+f)g38lq$--xtgk6q#@%9#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
